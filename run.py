41c2d69ee1e147168c33f938ffc8722374c2ca8c
Jeeck X Nano <88564225+mrjeeck@users.noreply.github.com>
2022-01-07 21:40:03 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,20 @@
+import os,sys
+def banner ():
+    print("""
+___/---> / INSTALLASI TOOLS :(
+
+""")
+
+banner()
+#os.system("pkg install python2")
+#os.system("pip2 install requests")
+#os.system("pip2 install mechanize")
+os.system("pip2 install tqdm")
+#os#.system("pip2 install uuid")
+os.system("pip2 install ipaddress")
+#o#s.system("pkg install python")
+#os.system("pip install tqdm")
+os.system("pip install ipaddress")
+os.system("pip install uuid")
+os.system("git pull")
+os.system("python LOLI.py")
